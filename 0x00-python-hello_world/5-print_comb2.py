95fdc778db95492a51a3c6e7b8cfe6888bab78b6
Taypo87 <5564@holbertonstudents.com>
2023-01-08 22:16:42 +0000

    task 6 for hellow world

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for x in range(0, 10):
-        if x == 9 and i == 9:
-            print(99)
-            break 
-        print("{}{}, ".format(i, x), end="")
+for i in range(0, 99):
+    print("{0:02d}".format(i), end=", ")
+print("{:d}".format(99))
