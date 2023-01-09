f3536c7a33bae6517e6e33114b9a856cb479bd9a
Taypo87 <5564@holbertonstudents.com>
2023-01-09 19:00:16 +0000

    updated main check

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-count = (len(sys.argv) - 1)
-if count > 1:
-    print("{:d} arguments:".format(count))
-else:
-    print("{:d} argument:".format(count))
-for i in range(1, count + 1):
-    print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    count = (len(sys.argv) - 1)
+    if count > 1:
+        print("{:d} arguments:".format(count))
+    else:
+        print("{:d} argument:".format(count))
+    for i in range(1, count + 1):
+        print("{}: {}".format(i, sys.argv[i]))
