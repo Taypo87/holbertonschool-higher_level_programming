a484d19381ebb174f2bbc049017a8f0150edbc67
Taypo87 <5564@holbertonstudents.com>
2023-01-10 12:01:40 +0000

    fixed handling of negative nuumbers

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-digit = (number % 10)
-if digit > 5:
+if number < 0:
+    number = (number * -1)
+    digit = (number % 10)
+    digit = (digit * -1)
+else:
+    digit = (number % 10)
+if digit < 0:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, digit))
+elif digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, digit))
 elif digit == 0:
     print("Last digit of {} is {} and is 0".format(number, digit))
