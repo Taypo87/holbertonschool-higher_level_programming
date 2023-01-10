0f55173eab42b0f80d4ccef6671499b7d7801d9a
Taypo87 <5564@holbertonstudents.com>
2023-01-10 12:09:19 +0000

    fixed handling of negative numbers, reverted oringal number back to negative

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 import random
-n = random.randint(-10000, 10000)
-if n < 0:
-    n = (n * -1)
-    d = (n % 10)
-    d = (d * -1)
-    n = (n * -1)
+number = random.randint(-10000, 10000)
+if number < 0:
+    number = (number * -1)
+    digit = (number % 10)
+    digit = (digit * -1)
+    number = (number * -1)
 else:
-    d = (n % 10)
-if d < 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(n, d))
-elif d > 5:
-    print("Last digit of {} is {} and is greater than 5".format(n, d))
-elif d == 0:
-    print("Last digit of {} is {} and is 0".format(n, d))
+    digit = (number % 10)
+if digit < 0:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, digit))
+elif digit > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, digit))
+elif digit == 0:
+    print("Last digit of {} is {} and is 0".format(number, digit))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(n, d))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, digit))
