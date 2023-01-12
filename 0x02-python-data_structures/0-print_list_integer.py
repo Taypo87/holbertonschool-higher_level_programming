0d95392f739daaf97771c94585dd0c33341cca23
Taypo87 <5564@holbertonstudents.com>
2023-01-12 11:09:53 +0000

    fixed whitespace error

--- 0x02-python-data_structures/0-print_list_integer.py
+++ 0x02-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     index = (len(my_list) + 1)
-    for x in range (0, (index)):
+    for x in range(0, (index)):
         print("{}".format(x))
