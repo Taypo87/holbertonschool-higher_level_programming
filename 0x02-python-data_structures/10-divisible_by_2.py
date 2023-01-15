29a725eb203a847be875b1b0a98406873b109d23
Taypo87 <5564@holbertonstudents.com>
2023-01-15 13:12:58 +0000

    task 10 for python data structures

---
+++ 0x02-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,10 @@
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+    new_list = my_list[:]
+    for x in new_list:
+        if (x % 2 == 0):
+            new_list[x] = True
+        else:
+            new_list[x] = False
+    return new_list
