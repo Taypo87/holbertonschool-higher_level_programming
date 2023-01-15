a326138e1555ee22a0112b0d2a9dfb6ab16e55ae
Taypo87 <5564@holbertonstudents.com>
2023-01-15 12:03:09 +0000

    task 8, fixed empty string handling

--- 0x02-python-data_structures/8-multiple_returns.py
+++ 0x02-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@ def multiple_returns(sentence):
     strlen = len(sentence)
     firstl = sentence[:1]
     if sentence is "":
-        first = None
+        firstl = None
     tuple_ret = strlen, firstl
     return(tuple_ret)
