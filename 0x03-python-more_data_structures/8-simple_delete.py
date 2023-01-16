275d345caa972deb9ff5622c91a8190b5d0ce3d6
Taypo87 <5564@holbertonstudents.com>
2023-01-16 14:18:32 +0000

    task 8 for more python data structures

--- 0x03-python-more_data_structures/8-simple_delete.py
+++ 0x03-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary == True:
+    if key in a_dictionary is True:
         a_dictionary.pop(key)
     return a_dictionary
