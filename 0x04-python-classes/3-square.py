2a8f9135955e5581fc5ce611c1191182cb1b6c01
Taypo87 <5564@holbertonstudents.com>
2023-01-17 23:18:36 +0000

    task 3 for python classes

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -14,16 +14,18 @@ class Square:
     @property
     def size(self):
         return self. __size
-        
+
     @size.setter
-    def size(self,size):
+    def size(self, size):
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     """
     Return area of a square
     """
+
     def area(self):
         return (self.__size * self.__size)
