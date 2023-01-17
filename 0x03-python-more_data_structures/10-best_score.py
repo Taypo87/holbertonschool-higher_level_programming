b65f9e99c1912cf646e64ef313d484ab18800a51
Taypo87 <5564@holbertonstudents.com>
2023-01-17 13:28:12 +0000

    task 10 for more python data structures

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     biggest = 0
     for key, value in a_dictionary.items():
