3f0240ef249c591b779c8281c304c17c43b5b94c
Taypo87 <5564@holbertonstudents.com>
2023-01-17 21:20:41 +0000

    task 0 for python classes

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,13 +2,13 @@
 
 """Class documentation"""
 
+
 class Square:
     """class Square with private instance attribute size
     """
     pass
 
-
     def __init__(self, size=0):
         """ init method of class Square
         """
-        self.__size = size 
+        self.__size = size
