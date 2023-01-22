88a54d997fce4a03707be7e02e82a9d76792a02c
Taypo87 <5564@holbertonstudents.com>
2023-01-22 12:31:28 +0000

    added height setter...

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     def width(self, width):
         """method: set_width setter
         """
-        if (not isinstance(self.__width, int)) or isinstance(self.__width, bool):
+        if (not isinstance(self.__width, int)):
             raise TypeError("width must be an integer")
         if self.__width < 0:
             raise ValueError("width must be >= 0")
@@ -35,10 +35,18 @@ class Rectangle:
 
     @property
     def height(self, height):
-        """method: set height setter
+        """method: set height getter
         """
-        if not isinstance(self.height, int) or isinstance(self.__height, bool):
+        if not isinstance(self.height, int):
             raise TypeError("height must be an integer")
         if self.__height < 0:
             raise ValueError("height must be >= 0")
         self.__height = height
+
+    @height.setter
+    def height(self, height):
+        """method: set_height setter"""
+        if (not isinstance(self.__height, int)):
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
