94c4c5cb06afa62f3fbf6d040b7b2e0fda9092d2
Taypo87 <5564@holbertonstudents.com>
2023-01-27 10:44:23 +0000

    fixed documentation

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """function is_same_class reurns true or false"""
 
+
 def is_same_class(obj, a_class):
     """checks if object is an instance of Class"""
     if isinstance(obj, a_class):
