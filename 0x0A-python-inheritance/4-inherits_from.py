fbb7a439dfdf976e7dd20b4e34e63ad0d4d1624e
Taypo87 <5564@holbertonstudents.com>
2023-01-27 11:22:48 +0000

    task 4 for python inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,5 +7,7 @@ def inherits_from(obj, a_class):
     """compares instance to class and subclass"""
     if isinstance(obj, a_class) or issubclass(obj, a_class) is True:
         return True
+    if type(obj) is type(a_class):
+        return True
     else:
         return False
