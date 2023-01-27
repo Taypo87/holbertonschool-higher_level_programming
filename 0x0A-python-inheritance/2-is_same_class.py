62e3248e31634242298caa2f78a7fdd60de2253d
Taypo87 <5564@holbertonstudents.com>
2023-01-27 10:47:55 +0000

    changed how it checks for type

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -4,7 +4,7 @@
 
 def is_same_class(obj, a_class):
     """checks if object is an instance of Class"""
-    if isinstance(obj, a_class):
+    if type(obj) == a_class:
         return True
     else:
         return False
