40b6c1b35c7fcbc0c4f047e9cee44f16ade15209
Taypo87 <5564@holbertonstudents.com>
2023-01-27 11:19:12 +0000

    task 4 for python inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Method that checks for both direct or indirect inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """compares instance to class and subclass"""
+    if (isinstance(obj, a_class) or issubclass(obj, a_class)) == True:
+        return True
+    else:
+        return False
