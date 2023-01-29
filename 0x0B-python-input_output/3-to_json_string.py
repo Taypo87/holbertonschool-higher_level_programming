1ebd6417d6fc80e9e7d5a2fa5d8a72df6d8f5110
Taypo87 <5564@holbertonstudents.com>
2023-01-29 19:16:20 +0000

    task 3 for python input output

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import json
+"""Task 3 for python input output"""
+
+
+def to_json_string(my_obj):
+    """function returns converted JSON string"""
+
+    return json.dumps(my_obj)
