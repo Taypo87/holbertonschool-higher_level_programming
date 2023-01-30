7e5d6d945a4594121ff34155365760e863d6e32c
Taypo87 <5564@holbertonstudents.com>
2023-01-30 12:17:13 +0000

    task 9 for python input output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,12 +7,12 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """method init"""
-        
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-     def to_json(self):
+    def to_json(self):
         '''method to_json'''
 
         return self.__dict__
