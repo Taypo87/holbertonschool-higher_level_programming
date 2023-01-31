e1124446f126e0df673e99aee21f33aa9d74b907
Taypo87 <5564@holbertonstudents.com>
2023-01-31 15:01:37 +0000

    starting square subclass

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,10 +9,22 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """size setter"""
+        if type(size) is not int:
+            raise TypeError("height must be an integer")
+        if size <= 0:
+            raise ValueError("height must be > 0")
+        self.__size = size
 
 def __str__(self):
         """String Informal of the Rectangle"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
                                                          self.__x,
                                                          self.__y,
-                                                         self.__height)
+                                                         self.__size)
