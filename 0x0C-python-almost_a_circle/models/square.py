5ae7e99d96cef658d37e07dcf80bd6a504f3f306
Taypo87 <5564@holbertonstudents.com>
2023-01-31 14:48:01 +0000

    starting square subclass

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class square inherits from rectangle"""
+
+from models.rectangle import Rectangle
+
+
+
+class Square(Rectangle):
+    """Represnts a square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
