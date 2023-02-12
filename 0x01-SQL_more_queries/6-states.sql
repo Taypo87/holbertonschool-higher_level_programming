baf68effb800a3f9babdd95d50c3f1c329c35d0f
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-12 07:47:00 -0800

    task 6 for mysql

--- 0x01-SQL_more_queries/6-states.sql
+++ 0x01-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- creates a new database called hbtn_0d_usa and adds the table states
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT
 name VARCHAR(256) NOT NULL, UNIQUE(id), PRIMARY KEY(id));
