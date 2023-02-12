afa44a8b6bfd089e3cec7491b64e1ebc6ed8cbcd
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-12 07:56:02 -0800

    task 7 for mysql queries

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,6 @@
+-- creates a new database called hbtn_0d_usa and adds the table citites
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (id INT NOT NULL AUTO_INCREMENT,
+state_id INT NOT NULL, name VARCHAR(256) NOT NULL, UNIQUE(id), PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
