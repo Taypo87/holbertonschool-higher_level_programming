abcc4d07a57a2b622063bf9412fd12ff4ef35136
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-23 12:33:50 -0800

    task 1 for python object relational database

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" Connects to and queries """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], database=argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name like 'n%' ORDER BY id ASC")
+    fetch = cur.fetchall()
+    for row in fetch:
+        print(row)
+    cur.close()
+    db.close()
