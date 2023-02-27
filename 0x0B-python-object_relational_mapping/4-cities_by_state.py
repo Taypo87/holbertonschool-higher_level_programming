82b95295a93004f9c237fab7894986a4acffa1b6
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-27 02:46:50 -0800

    task 4 for ORM

---
+++ 0x0B-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+""" Connects to and queries """
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], database=argv[3])
+    cur = db.cursor()cur = db.cursor()
+    cur .execute(
+        "cities.id, cities.name, states.name FROM cities join states ON cities.state_id = states.id;")
+    fetch = cur.fetchall()
+    for row in fetch:
+        print(row)
+    cur.close()
+    db.close()
+    
