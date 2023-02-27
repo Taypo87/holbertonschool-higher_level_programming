a4235109917caa6c79694cd4da545cf6c8868d70
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-27 09:44:15 -0800

    model state

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 """ Module for state class and base instance"""
 
 
@@ -8,8 +9,9 @@ from sqalchemy.ext.declaritve import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """State class"""
-    __tablename__= 'states'
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
