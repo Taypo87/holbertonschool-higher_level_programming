a6e561c26ed20dec307693ab2fa0a7e59380d899
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-27 04:40:17 -0800

    task 5 for ORM

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], database=argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT cities.name FROM cities 
-        JOIN states ON states.id = cities.state_id 
+    cur.execute("""SELECT cities.name FROM cities
+        JOIN states ON states.id = cities.state_id
         WHERE states.name LIKE %s""", (argv[4],))
     fetch = cur.fetchall()
     print(", ".join(row[0] for row in fetch))
