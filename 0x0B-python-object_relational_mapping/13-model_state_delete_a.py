8022c493b578e51e20543b8f52310172195a4e9f
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-28 10:34:34 -0800

    task 13 for ORM

---
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Lists state object that matches argument passed """
+
+import sqlalchemy
+from sqlalchemy import create_engine, select, update, delete
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    rmstate = session.query(State).where(State.name.contains('a'))
+    
+    for row in rmstate:
+        session.delete(row)
+    session.commit()
+    session.close()
