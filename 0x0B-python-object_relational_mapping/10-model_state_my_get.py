e8db92fc5b8eb94bb75c0cb42424c62183e1aca0
Taypo87 <taylor.poczynek87@gmail.com>
2023-02-28 03:26:56 -0800

    task 10 for ORM

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Lists state object that matches argument passed """
+
+import sqlalchemy
+from sqlalchemy import create_engine, select
+from sys import argv
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = select(State).where(State.name.like('{}'.format(argv[4])))
+    results = engine.execute(query).fetchall()
+    for record in results:
+        print("{}".format(record.id))
+    session.close()
