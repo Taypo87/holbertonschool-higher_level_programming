12c2511df2750bb0fc3f959ba439da97240560c7
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-10 18:27:22 +0000

    fixed variable name

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
 
     print('Body response:$')
-    print('\t- type: <{}>'.format(type(response)))
-    print('\t- content: {}'.format(response))
-    print('\t- utf8 content: {}'.format(response.decode('utf-8')))
+    print('\t- type: <{}>'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('utf-8')))
