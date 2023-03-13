059920e98143b6626624d3a1050d048cf2152913
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 15:08:56 +0000

    task 8 for python network

--- 0x0C-python-network_1/8-json_api.py
+++ 0x0C-python-network_1/8-json_api.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     else:
         q = ""
 
-    response = post("http://0.0.0.0:5000/search_user", data=q)
+    response = post("http://0.0.0.0:5000/search_user", data={'q': q})
     try:
         my_dict = response.json()
         id = my_dict.get('id')
