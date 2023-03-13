b3c50251de70c3f0cb95f51446a3fc60d49eee04
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 12:52:05 +0000

    task 5 for python network

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -5,5 +5,6 @@
 from sys import argv
 import requests
 
-request = requests.get(argv[1])
-print(request.headers.get("X-Request-Id"))
+if __name__ == '__main__':
+    request = requests.get(argv[1])
+    print(request.headers.get("X-Request-Id"))
