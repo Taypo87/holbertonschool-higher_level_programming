d2e9a521f04df6796cd15f7acc9a82d9831e1e34
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 10:54:44 +0000

    changed comment

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-# Requests a url and displays response header value
+""" Script that takes in a URL and displays the value of the X-Request-Id """
+
 import urllib
 import sys
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    print(response.info()['X-Request-Id'])
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        print(response.info()['X-Request-Id'])
