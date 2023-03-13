ceaf03b6ad2c8937a2c6cd8b8907da9766de3d3c
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 12:24:40 +0000

    task 3 for python network

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -3,12 +3,12 @@
 """ Send request to a url then displays the bnody of the response """
 
 from urllib import request, error
-from sys import argv 
+from sys import argv
 
 if __name__ == "__main__":
     req = request.Request(argv[1])
     try:
-      with request.urlopen(req) as response:
-         print(response.read().decode("utf-8"))
+        with request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
     except error.HTTPError as err:
         print("Error code: {}".format(err.code))
