df3a2f056f7e9763379248d92b4baafb2a76965d
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 10:57:56 +0000

    changed comment

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Script that takes in a URL and displays the value of the X-Request-Id """
 
-import urllib
+import urllib.request
 import sys
 if __name__ == "__main__":
+
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.info()['X-Request-Id'])
