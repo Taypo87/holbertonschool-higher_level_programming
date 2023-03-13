dbd2888da018de0b81bc63ab3a12469465d58e3c
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 12:51:06 +0000

    task 5 for python network

--- 0x0C-python-network_1/5-hbtn_header.py
+++ 0x0C-python-network_1/5-hbtn_header.py
@@ -2,10 +2,8 @@
 
 """ Script that takes in a URL and displays the value of the X-Request-Id """
 
-
 from sys import argv
 import requests
-
 
 request = requests.get(argv[1])
 print(request.headers.get("X-Request-Id"))
