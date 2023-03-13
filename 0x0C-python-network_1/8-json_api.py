81fa92a5ba0103fb91908369284e2767cf06afbb
Taypo87 <taylor.poczynek87@gmail.com>
2023-03-13 15:04:54 +0000

    task 8 for python network

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" Script that takes in a letter and sends a post request """
+
+from sys import argv
+from requests import get, post
+
+if __name__ == '__main__':
+    if argv[1]:
+        q = argv[1]
+    else:
+        q = ""
+
+    response = post("http://0.0.0.0:5000/search_user", data=q)
+    try:
+        my_dict = response.json()
+        id = my_dict.get('id')
+        name = my_dict.get('name')
+        if len(my_dict) == 0 or not id or not name:
+            print("No result")
+        else:
+            print("[{}] {}".format(my_dict.get('id'), my_dict.get('name')))
